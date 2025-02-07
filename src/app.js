6f9e5ae6e1c64220d1cb353e64ccd7bd03b88487
Frontend-kik <153603429+Frontend-kik@users.noreply.github.com>
2025-02-07 12:08:21 +0200

    Stateful и stateless-компоненты

--- src/app.js
+++ src/app.js
@@ -1,30 +1,32 @@
-import  {useState} from 'react';
+import {useState} from "react";
 import styles from './app.module.css';
 
-const Counter =({value, setValue}) => {
-    return (
-        <>
-            <div>{value}</div>
-            <button onClick={() => setValue(value +1)}>{value}+ 1</button>
-        </>
-    );
-};
+const AppLayout = ({a, b, setA, setB, sum}) => (
+
+    <div className={styles.app}>
+      <div> A: {a}</div>
+        <button onClick={() => setA(a + 1)}>Прибавить 1 к А</button>
+        <div> B:{b}</div>
+        <button onClick={() => setB(b + 1)}>Прибавить 1 к B</button>
+        <div>Сумма A + B: {sum}</div>
+    </div>
+)
 
 export const App = () => {
-    const [value, setValue] = useState(0);
+    const [a, setA] = useState(0);
+    const [b, setB] = useState(0);
 
-    return (
-        <div className={styles.app}>
-            <label>Счетчик: {value}</label>
-            <Counter value={value} setValue={setValue}/>
-        </div>
-    );
+    const sum = a + b;
+
+    return <AppLayout a={a} b={b} setA={setA} setB={setB} sum={sum} />
 };
 
-// Single source of truth - в компоненте могут использоваться данные из разных источнков
-// Разные источники обновляются по разному
-// Источником из двух состояний выбираем то , что выше(родительский)
-// Мнемонизация - способность сохранять возвращаемые данные в память
+// {/*// stateless - не содержащий состояние*/
+// }
+// {/*// Stateful - содержащий состояние*/
+// }
+
+
 
 
 
